481e19d5f4eea4226f87a0c2ef9f53b291c1478e
Mustafif <mustafif0929@gmail.com>
2023-11-19 19:27:02 -0500

    added fixed_buffer implementation

--- ch06/serde.zig
+++ ch06/serde.zig
@@ -1,20 +1,16 @@
 const std = @import("std");
 const json = std.json;
-
 /// A generic structure to Serialize/Deserialize a type
 pub fn SerdeJSON(comptime T: type) type {
     return struct {
         allocator: std.mem.Allocator,
         val_string: []const u8 = undefined,
-        val: std.json.Parsed(T) = undefined,
-
+        val: json.Parsed(T) = undefined,
         const Self = @This();
-
         /// Initialize given an allocator to use
         pub fn init(allocator: std.mem.Allocator) Self {
             return .{ .allocator = allocator };
         }
-
         /// Deinitalizes the values created
         pub fn deinit(self: *Self) void {
             self.allocator.free(self.val_string);
@@ -23,18 +19,22 @@ pub fn SerdeJSON(comptime T: type) type {
 
         /// Deserialized a string into the type `T` given
         pub fn deser(self: *Self, string: []const u8) !void {
-            const parse = try json.parseFromSlice(T, self.allocator, string, .{});
-            self.val = parse;
+            self.val = try json.parseFromSlice(T, self.allocator, string, .{});
         }
+        /// Returns the inner value from `json.Parsed(T)`
+        pub fn value(self: Self) T {
+            return self.val.value;
+        }
+
         // zig fmt: off
         /// Serializes a value into a JSON string
         pub fn ser(self: *Self, val: T) !void {
-            const string = try json.stringifyAlloc(self.allocator, val, 
+            self.val_string = try json.stringifyAlloc(self.allocator, val, 
             .{ .emit_strings_as_arrays = false, .whitespace = .indent_tab });
-            self.val_string = string;
         }
     };
 }
+
 // zig fmt: on
 // A structure to represent a Person
 const Person = struct {
@@ -43,9 +43,11 @@ const Person = struct {
 
     const Self = @This();
 
+    /// Returns a default Person
     pub fn default() Self {
         return .{ .name = "Person", .age = 20 };
     }
+    /// Checks if two Persons are equal
     pub fn equal(a: Self, b: Self) bool {
         return std.mem.eql(u8, a.name, b.name) and a.age == b.age;
     }
@@ -69,6 +71,7 @@ pub fn main() !void {
     try person_sj.ser(person);
     std.debug.print("Serialized:\n{s}\n", .{person_sj.val_string});
     try person_sj.deser(person_sj.val_string);
-    std.debug.print("Deserialized:\n\t{any}\n", .{person_sj.val.value});
-    std.debug.print("Equal? {}\n", .{Person.equal(person, person_sj.val.value)});
+    std.debug.print("Deserialized:\n\t{any}\n", .{person_sj.value()});
+    std.debug.print("Equal? {}\n", .{Person.equal(person, person_sj.value())});
 }
+
