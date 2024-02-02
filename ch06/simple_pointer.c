30e1fa1dc84c57a0fd6b2e5668d4cb1696e0a8a8
Mustafif <mustafif0929@gmail.com>
2024-02-01 20:31:12 -0500

    update

--- ch06/simple_pointer.c
+++ ch06/simple_pointer.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 
-int main(){
+int main()
+{
     int val = 45;
     int* ptr = &val;
-
     printf("Val = %d\n", val);
     printf("Memory address of val: %p\n", ptr);
     printf("Value of ptr = %d\n", *ptr);
-
     return 0;
 }
